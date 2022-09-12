7cb7f644c41760a41d642127bb170e48354b180b
Dobby Has No Master No More <alexheinking71@gmail.com>
2022-09-12 15:59:26 -0500

    test

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -15,7 +15,7 @@ import { ReactPhotoCollage } from "react-photo-collage"
   img = [
     "https://instagram.fmkc1-1.fna.fbcdn.net/v/t51.2885-15/298490739_3225646834352421_4255205146254951782_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=instagram.fmkc1-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=9l7VlXhskwkAX_W7mdX&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=MjkwMDM5NjQ5MjYxNjk5NDg1MA%3D%3D.2-ccb7-5&oh=00_AT8mzI2Z3nq7snVNQo5-vP9BoPEnH31XcmVP3FQ4YiHHIA&oe=62FA9D19&_nc_sid=30a2ef"
   ]
-  setting = {
+  settings = {
     width: '800px',
     height: ['250px', '170px'],
     layout: [1, 4],
@@ -31,11 +31,12 @@ import { ReactPhotoCollage } from "react-photo-collage"
     ],
     showNumOfRemainingPhotos: true
   };
+  //nigga
 
   render() {
     return (
     <div>
-      <ReactPhotoCollage {...this.setting} />
+      <ReactPhotoCollage {...this.settings} />
       <img src={this.img[0]} />
      </div>);
 
